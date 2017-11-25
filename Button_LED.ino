272dacbb55323b43c3cc263e81d44e5b818d20e8
Sanjaye and Sid <sanjayesn@gmail.com>
2017-11-24 20:42:17 -0800

    Added button LED circuit

---
+++ Button_LED.ino
@@ -0,0 +1,19 @@
+#include <Button.h>
+
+int state = LOW;
+int ledPin = 7;
+Button ledButton = Button(8, PULLDOWN);
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if(ledButton.uniquePress()) {
+    state = (state ==  HIGH) ? LOW : HIGH;
+  }
+  
+  digitalWrite(ledPin, state);
+}
+
+  
